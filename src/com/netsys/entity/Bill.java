fc2136f7dc74b6cfe343ef5374ba22117ab45c35
NamMai195 <mnam3239@gmail.com>
2024-07-26 12:29:13 +0700

    update entity_bill

--- src/com/netsys/entity/Bill.java
+++ src/com/netsys/entity/Bill.java
@@ -5,56 +5,79 @@
 package com.netsys.entity;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  *
  * @author Nam
  */
 public class Bill {
-    private int mabillD;
-    private int makh;
-    private int manv;
-    private Double tong;
-    private LocalDateTime tglap;
+    private int mabill;
+    private String makh;
+    private String manv;
+    private Double Tong;
+    private Date tglap;
+    private boolean trangthai;
 
-    public int getMabillD() {
-        return mabillD;
+    public Bill() {
     }
 
-    public void setMabillD(int mabillD) {
-        this.mabillD = mabillD;
+    public Bill(int mabill, String makh, String manv, Double Tong, Date tglap, boolean trangthai) {
+        this.mabill = mabill;
+        this.makh = makh;
+        this.manv = manv;
+        this.Tong = Tong;
+        this.tglap = tglap;
+        this.trangthai = trangthai;
     }
 
-    public int getMakh() {
+    public int getMabill() {
+        return mabill;
+    }
+
+    public void setMabill(int mabill) {
+        this.mabill = mabill;
+    }
+
+    public String getMakh() {
         return makh;
     }
 
-    public void setMakh(int makh) {
+    public void setMakh(String makh) {
         this.makh = makh;
     }
 
-    public int getManv() {
+    public String getManv() {
         return manv;
     }
 
-    public void setManv(int manv) {
+    public void setManv(String manv) {
         this.manv = manv;
     }
 
     public Double getTong() {
-        return tong;
+        return Tong;
     }
 
-    public void setTong(Double tong) {
-        this.tong = tong;
+    public void setTong(Double Tong) {
+        this.Tong = Tong;
     }
 
-    public LocalDateTime getTglap() {
+    public Date getTglap() {
         return tglap;
     }
 
-    public void setTglap(LocalDateTime tglap) {
+    public void setTglap(Date tglap) {
         this.tglap = tglap;
     }
+
+    public boolean isTrangthai() {
+        return trangthai;
+    }
+
+    public void setTrangthai(boolean trangthai) {
+        this.trangthai = trangthai;
+    }
+    
 
 }
